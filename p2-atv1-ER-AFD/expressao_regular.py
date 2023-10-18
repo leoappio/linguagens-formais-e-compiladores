49687e18076a501e0e8ed9d6c9cc6fa4d25a6073
leo.appio <leonardo.appio@gmail.com>
2023-10-18 00:46:24 -0300

    iniciada implementacao da referencia dos nos ao lado

--- p2-atv1-ER-AFD/expressao_regular.py
+++ p2-atv1-ER-AFD/expressao_regular.py
@@ -5,7 +5,15 @@ class ExpressaoRegular():
         self.nos = self.__ler_input(input)
 
         for no in self.nos:
-            print(no.simbolo, no.numero)
+            simbolo_esquerda = None
+            if no.no_esquerda != None:
+                simbolo_esquerda = no.no_esquerda.simbolo
+
+            simbolo_direita = None
+            if no.no_direita != None:
+                simbolo_direita = no.no_direita.simbolo
+
+            print(simbolo_esquerda,'-',no.simbolo, no.numero, '-', simbolo_direita)
 
     def __ler_input(self, input):
         input += '#'
@@ -42,5 +50,11 @@ class ExpressaoRegular():
                 next_char = input[index + 1]
                 if next_char not in operadores and next_char != parenteses[1]:
                     lista_nos.append(No('.'))
+        
+        for index, no in enumerate(lista_nos):
+            if index < len(lista_nos) - 1:
+                prox_no = lista_nos[index + 1]
+                no.no_direita = prox_no
+                prox_no.no_esquerda = no
 
         return lista_nos
